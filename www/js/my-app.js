846d1157d6842e65efb7c8fc21e7ed4d36ae0aee
JoaoOliveira14 <joliveira055@gmail.com>
2018-06-04 23:32:56 -0300

    Nova versão do aplicativo

---
+++ www/js/my-app.js
@@ -0,0 +1,80 @@
+
+var app = new Framework7({
+  // App root element
+  root: '#app',
+  // App Name
+  name: 'My App',
+  // App id
+  id: 'com.myapp.test',
+  // Enable swipe panel
+  panel: {
+    swipe: 'left',
+  },
+  // Add default routes
+  routes: [
+       {
+      path: '/onibus/',
+      url: 'paginas/onibus.html',
+    },
+         {
+      path: '/pontos/',
+      url: 'paginas/paradas.html',
+    },
+         {
+      path: '/info/',
+      url: 'paginas/info.html',
+    },
+      
+          // linhas de ônibus
+         {
+      path: '/linha1/',
+      url: 'paginas/linhas/linha1.html',
+    },
+           {
+      path: '/linha2/',
+      url: 'paginas/linhas/linha2.html',
+    },
+           {
+      path: '/linha3/',
+      url: 'paginas/linhas/linha3.html',
+    },
+           {
+      path: '/linha4/',
+      url: 'paginas/linhas/linha4.html',
+    },
+           {
+      path: '/linha5/',
+      url: 'paginas/linhas/linha5.html',
+    },
+           {
+      path: '/linha6/',
+      url: 'paginas/linhas/linha6.html',
+    },
+           {
+      path: '/linha7/',
+      url: 'paginas/linhas/linha7.html',
+    },
+           {
+      path: '/linha8/',
+      url: 'paginas/linhas/linha8.html',
+    },
+           {
+      path: '/linha9/',
+      url: 'paginas/linhas/linha9.html',
+    },
+           {
+      path: '/linha10/',
+      url: 'paginas/linhas/linha10.html',
+    },
+      
+  ],
+  // ... other parameters
+});
+
+var mainView = app.views.create('.view-main');
+
+
+
+
+
+
